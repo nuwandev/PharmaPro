b6243e96f9abc40c8c0807d344dc5a38b4004e62
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-03-24 16:32:00 +0530

    Add database and configuration dependencies; update module exports and main class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module com.nuwandev.pharmapro {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.zaxxer.hikari;
+    requires spring.jdbc;
+    requires java.sql;
+    requires flyway.core;
+    requires io.github.cdimascio.dotenv.java;
 
+    opens com.nuwandev.pharmapro.app to javafx.graphics;
 
-    opens com.nuwandev.pharmapro to javafx.fxml;
-    exports com.nuwandev.pharmapro;
+    opens com.nuwandev.pharmapro.ui to javafx.fxml;
+    exports com.nuwandev.pharmapro.ui;
+
+    opens db.migration;
 }
